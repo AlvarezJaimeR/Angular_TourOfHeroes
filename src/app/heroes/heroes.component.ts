3c5f8bd6e9dc4d032b951d219894119867970514
Jaime Alvarez <jrralvarez@gmail.com>
2021-10-06 21:37:25 -0500

    updated the heroes component in order to import from the hero service instead heroes import.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,23 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }; */
 
-  heroes = HEROES;
+/*   heroes = HEROES; */
+
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
 }
